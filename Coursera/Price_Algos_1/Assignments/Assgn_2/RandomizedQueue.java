ffa98d5a832ffcb330278a1eb1adaf47cff25203
Chakrapani <chakrapani.r@gmail.com>
2015-02-17 08:20:51 -0800

    Assignment 2 first cut

---
+++ Coursera/Price_Algos_1/Assignments/Assgn_2/RandomizedQueue.java
@@ -0,0 +1,98 @@
+/**********************************************************
+  * Randomzied Queue implementation with resizing array
+  * 
+  ********************************************************/
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    private Item[] q;             //array of items
+    private int N = 0;           // number of elements in the queue
+    private int last = 0;        // index of the last element in the queue
+    
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+        q = (Item[]) new Object[2];
+    }
+    
+    // is the queue empty?
+    public boolean isEmpty() {
+        return N == 0;
+    }
+    
+    // return the number of items on the queue
+    public int size() {
+        return N;
+    }
+    
+    private void resizeArray(int capacity) {
+        assert capacity >= N;
+        Item[] temparr = (Item[]) new Object[capacity];
+        for(int i = 0; i < N; i++) {
+            temparr[i] = q[i];
+        }
+        q = temparr;
+    }
+    
+    // add the item
+    public void enqueue(Item item) {
+        if(N == q.length)  resizeArray(2*q.length);
+        q[last++] = item;
+        N++;
+    }
+    
+    // remove and return a random item
+    public Item dequeue() {
+        if(isEmpty())  throw new NoSuchElementException("Queue Underflow");
+        int randidx = StdRandom.uniform(N);
+        Item item = q[randidx];
+        if(randidx != (last-1))  q[randidx] = q[last-1];
+        q[last-1] = null;
+        last--;
+        N--;
+        // Shrink the array when number of elements is one-quarter of array length
+        if(N > 0 && N == q.length/4)  resizeArray(q.length/2);
+        return item;
+    }
+    
+    // return (but do not remove) a random item
+    public Item sample() {
+        if(isEmpty())  throw new NoSuchElementException("Queue Underflow");
+        int randidx = StdRandom.uniform(N);
+        Item item = q[randidx];
+        return item;
+    }
+    
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+        return new RandomQueueIterator();
+    }
+    
+    private class RandomQueueIterator implements Iterator<Item> {
+        private int i = 0;
+        private Item[] itrarr;
+        
+        public RandomQueueIterator() {
+            itrarr = (Item[]) new Object[N];
+            for(int j = 0; j < N; j++) 
+                itrarr[j] = q[j];
+            StdRandom.shuffle(itrarr);
+        }
+        
+        public boolean hasNext() {
+            return i < N;
+        }
+        
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException("End of Queue");
+            Item item = itrarr[i++];
+            return item;
+        }
+    }
+}
+    
---
+++ Coursera/Price_Algos_1/Assignments/Assgn_2/Subset.java
@@ -0,0 +1,40 @@
+/**************************************************************
+  * A client program that takes a command line integer k;
+  * reads in a sequence of N strings from standard input 
+  * using StdIn.readString(); and prints out exactly k of them, 
+  * uniformly at random. Each item from the sequence can be 
+  * printed out at most once. You may assume that k is between
+  * 0 and N. Where N is the number of string on standard input. 
+  **************************************************************/
+
+public class Subset {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);  // Subset length from command line
+        int N = 0;                          // Current input sequence size
+        RandomizedQueue rq = new RandomizedQueue<String>();
+        
+        while(!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            N++;
+            //Enqueue when input sequence length is less than k
+            if(N <= k)  rq.enqueue(item);
+            else {
+                /* When input sequence length is greater than k, 
+                 * use reservoir sampling algorithm to choose 
+                 * the string to an existing evict and insert 
+                 * incoming string.
+                 */
+                 */
+                int randidx = StdRandom.uniform(N);
+                if(randidx < k) {
+                    rq.dequeue();
+                    rq.enqueue(item);
+                }
+            }
+        }
+        
+        while(!rq.isEmpty()) {
+            StdOut.println(rq.dequeue());
+        }
+    }
+}
